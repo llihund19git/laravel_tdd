e4b9c86612fc5738a1bb68d750371626fa37fdae
Dunhill Dimaapi <llihund19@gmail.com>
2020-02-18 23:31:20 +0800

    16-Notes-and-Policies

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -24,7 +24,7 @@
                                 @csrf
 
                                 <div class="flex">
-                                    <input value="{{ $task->body  }}" name="body" class="w-full {{ $task->completed ? 'text-gray-600' : ''}}">
+                                    <input value="{{ $task->body }}" name="body" class="w-full {{ $task->completed ? 'text-gray-600' : ''}}">
                                     <input type="checkbox" name="completed" onchange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
                                 </div>
                             </form>
@@ -41,7 +41,13 @@
                 <div>
                     <h2 class="text-lg text-gray-600 font-normal">General Notes</h2>
 
-                    <textarea class="card w-full" style="min-height: 200px;">Lorem ipsum</textarea>
+                    <form method="POST" action="{{ $project->path() }}">
+                        @csrf
+                        @method('PATCH')
+                        <textarea name="notes" class="card w-full mb-4" style="min-height: 200px;" placeholder="Anything special that you want to make a note of?">{{ $project->notes }}</textarea>
+
+                        <button type="submit" class="button">Save</button>
+                    </form>
                 </div>
             </div>
             <div class="lg:w-1/4 px-3">
